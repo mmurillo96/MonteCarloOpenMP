757cbdc17e6e975407e2bc9cf827911c866b0ed9
Murillo Cuervo Tarôuco <murillocuervot@gmail.com>
2021-05-30 21:09:40 -0300

    Update MonteCarloV2.c

--- MonteCarloV2.c
+++ MonteCarloV2.c
@@ -8,52 +8,51 @@ int main(int argc, char *argv[]) {
     //argumento 1 numero de threads
     //argumento 2 potencia
     
-    long i, numIn, n, potencia;
+    long i, contSucesso, totalPontos, potencia;
     unsigned int seed;
-    double x, y, pi;
+    double x, y, pi, start_time, end_time;
     long quantThread = atoi(argv[1]);
-    double start_time,end_time;
 
     omp_set_num_threads(atoi(argv[1]));//numero de threads
     potencia = atoi(argv[2]);//potencia
-    n = 10;
+    totalPontos = 10;
 
     for (i = 0; i < potencia; i++) {
-           n = n*10; 
+           totalPontos = totalPontos*10; 
     }
     
-    numIn = 0;
+    contSucesso = 0;
 
     start_time = omp_get_wtime();
 
     printf("-----------------------------------\n");
     printf("Potencia: %ld\n",potencia);
-    printf("Qtd de pontos Sorteados: %ld\n",n);
-    printf("Qtd de pontos Sorteados por Thread: %ld\n",n/quantThread);
+    printf("Qtd de pontos Sorteados: %ld\n",totalPontos);
+    printf("Qtd de pontos Sorteados por Thread: %ld\n",totalPontos/quantThread);
     printf("Quantidade de Threads: %ld\n",quantThread);
     printf("-----------------------------------\n");
 
-    #pragma omp parallel private(seed, x, y) reduction(+:numIn) 
+    #pragma omp parallel private(seed, x, y) reduction(+:contSucesso) 
     {
         //este calculo deve gerar seeds diferentes para cada execução.
         seed = 25234 + potencia * omp_get_thread_num();
-        
+
         //printf("Numero de Thread: %i\n",omp_get_thread_num());
         int thread = omp_get_thread_num();
-        long n_thread = n / quantThread;
+        long pontos_thread = totalPontos / quantThread;
         //printf("Quant Pontos Thread: %i\n",n_thread);
 
         #pragma omp for       
-            for (i = 0; i <= n_thread ; i++) {
+            for (i = 0; i <= pontos_thread ; i++) {
                 x = (double)rand_r(&seed) / RAND_MAX;
                 y = (double)rand_r(&seed) / RAND_MAX;
                 if (x*x + y*y <= 1){
-                    numIn++;
+                    contSucesso++;
                 }
             }        
     }
     
-    pi = 4.*(numIn*quantThread) / n;
+    pi = 4.*(contSucesso*quantThread) / totalPontos;
     
     end_time=omp_get_wtime();
 
